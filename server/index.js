f36ebc6db4e63f3e84dab9c69bfb01d214d5ede9
snehalpisale <snehalpisale409@gmail.com>
2026-01-23 16:52:07 +0530

    file changes

---
+++ server/index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import router from "./Routes/userRoute.js";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config(); 
+
+const app = express(); 
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: "*" }));
+
+app.use("/api", router);
+
+app.get("/snehal", (req, res) => {
+  res.send("Hello Everyone");
+});
+
+const PORT = process.env.PORT || 5000;
+const mongo_URL = process.env.MONGO_URL;
+
+mongoose
+  .connect(mongo_URL)
+  .then(() => {
+    console.log("MongoDB Connected Successfully");
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("MongoDB connection error:", error);
+  });
